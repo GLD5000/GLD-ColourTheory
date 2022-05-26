eef3139302a246314b8ab0ae27068e838f0030db
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-05-26 22:13:46 +0100

    Updated layout and JS
    Added colour-space conversions
    Updated colour picker wrapper and grid

--- script.js
+++ script.js
@@ -4,6 +4,120 @@ const color_picker_hex_label = document.getElementById("picker-label-hex");
 
 color_picker.onchange = function() {
 	color_picker_wrapper.style.backgroundColor = color_picker.value;    
-  color_picker_hex_label.innerHTML = color_picker.value;
+  color_picker_hex_label.innerHTML = 'Click To Copy: '+color_picker.value;
 }
 color_picker_wrapper.style.backgroundColor = color_picker.value;
+
+function hexToHSL(H) {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
+}
+
+function HSLToHex(h,s,l) {
+  s /= 100;
+  l /= 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+      m = l - c/2,
+      r = 0,
+      g = 0, 
+      b = 0; 
+
+  if (0 <= h && h < 60) {
+    r = c; g = x; b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x; g = c; b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0; g = c; b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0; g = x; b = c;
+  } else if (240 <= h && h < 300) {
+    r = x; g = 0; b = c;
+  } else if (300 <= h && h < 360) {
+    r = c; g = 0; b = x;
+  }
+  // Having obtained RGB, convert channels to hex
+  r = Math.round((r + m) * 255).toString(16);
+  g = Math.round((g + m) * 255).toString(16);
+  b = Math.round((b + m) * 255).toString(16);
+
+  // Prepend 0s, if necessary
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+function hueRotateHSL(hue,sat,lum, rotation){
+
+  return (hue + rotation > 360)? [hue + rotation - 360, sat, lum]: [hue + rotation, sat, lum]; 
+
+}
+
+function lumRotateHSL(hue,sat,lum, rotation){
+
+  rotation /= 360 * 100;
+
+  return (lum + rotation > 100)? [hue,sat,lum + rotation - 100]: [hue,sat,lum + rotation]; 
+
+}
+
+function hueRotateHEX(hex, rotation){
+
+  return hexToHSL(hex)[0]
+
+}
+
+function copyHex(){
+   /* Copy the text inside the text field */
+   navigator.clipboard.writeText(color_picker.value);
+
+   /* Alert the copied text */
+   alert("Copied the text: " + color_picker.value);
+}
+
