db6228efebdecf7c902810971178a14403e18958
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-06-17 17:35:52 +0000

    Add Hex to Hue function Add sat lum change to multi gradient

--- js/script.js
+++ js/script.js
@@ -271,11 +271,52 @@ console.log(counter());
 console.log(counterB(2));
 console.log(counterB(4));
 */
+function hexToHue(){
+
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+    return h;
+}
+
 function linearGradientMultiTone(hex){
   let luminance = 95;
   let lumAdjustment = 6;
-  
-  const variantDec = HSLlumGradient(hex,luminance,'*',0.93); 
+  const variantDec = HSLMultStrFixed(hex,1,1.05,0.93);
+  //const variantDec = HSLlumGradient(hex,luminance,'*',0.93); 
   const variant50 = lumChangeHEX(hex, luminance);
   /*
   luminance -= lumAdjustment;
