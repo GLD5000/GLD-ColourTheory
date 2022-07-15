14a989341d9b546cae6acafffc420a937e6c5b7f
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-07-15 07:57:30 +0100

    Add hsl combine to colour maker

--- js/modules/classes/colourmaker.js
+++ js/modules/classes/colourmaker.js
@@ -1,16 +1,15 @@
 export const colourMaker= () => {
-    console.log(`colour world is this`);
     const swatches = {
-        analogousA: {hue:{operation: 'add', value:-30},sat:{operation: 'keep', value: null}, lum:{operation: 'keep', value: null}},// use defaults or pipe || to avoid repetition
-        analogousB: ['hue', 30],
-        triadicA: ['hue', -120],
-        triadicB: ['hue', 120],
-        tetradicA: ['hue', 90],
-        tetradicB: ['hue', 180],
-        tetradicC: ['hue', 270],
-        monochromeA: ['lum', -10],
-        monochromeB: ['lum', 10],
-        neutral: ['sat', -200],
+        analogousA: {hue: -30, operation: 'add'},
+        analogousB: {hue: 30, operation: 'add'},
+        triadicA: {hue: -120, operation: 'add'},
+        triadicB: {hue: 120, operation: 'add'},
+        tetradicA: {hue: 90, operation: 'add'},
+        tetradicB: {hue: 180, operation: 'add'},
+        tetradicC: {hue: 270, operation: 'add'},
+        monochromeA: {lum: -10, operation: 'add'},
+        monochromeB: {lum: 10, operation: 'add'},
+        neutral: {sat: 0, operation: 'replace'}
       } ;
       const operations = {
         'multiply': (oldVal, newVal) =>  oldVal * newVal,
@@ -19,6 +18,17 @@ export const colourMaker= () => {
         'divide': (oldVal, newVal) =>  oldVal / newVal,
         'replace': (oldVal, newVal) =>  newVal,
         'keep': (oldVal, newVal) =>  oldVal
-      }
-      
+      } ;
+      const combineHSL = ({hue,sat,lum},{newHue = undefined, newSat = undefined, newLum = undefined, operation = 'add'}) => {
+        return {
+          hue: (!newHue)? hue: operations[operation](hue,newHue),
+          sat: (!newSat)? sat: operations[operation](sat,newlum),
+          lum: (!newLum)? lum: operations[operation](lum,newlum),
+        };
+      };
+      const testOldHsl = {hue: 1, sat: 2, lum: 3};
+      const testNewHsl = {newHue: 4, operation: 'divide'};
+      console.log(combineHSL(testOldHsl, testNewHsl));
+
 }
+
