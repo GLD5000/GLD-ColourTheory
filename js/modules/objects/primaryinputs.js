f49338c9e616bdcd659948905c662d6fb5612a2c
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-07-24 23:44:39 +0100

    Add primaryinputs function

--- js/modules/objects/primaryinputs.js
+++ js/modules/objects/primaryinputs.js
@@ -1,6 +1,6 @@
-export const primaryinputs = {
-    hueSlider = document.getElementById('hue-slider');
-    satSlider = document.getElementById('sat-slider');
-    lumSlider = document.getElementById('lum-slider');
-    picker = document.getElementById(name + '-picker');
-}
+export const makePrimaryInputs = () =>{ return {
+    hueSlider: document.getElementById('hue-slider'),
+    satSlider: document.getElementById('sat-slider'),
+    lumSlider: document.getElementById('lum-slider'),
+    picker: document.getElementById('primaryColour-picker'),
+}};
