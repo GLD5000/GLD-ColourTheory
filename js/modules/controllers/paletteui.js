80dd828b55b18c6af6b0d43d4fda89266a71723c
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-07-27 11:07:38 +0100

    Add oninput to paletteUi

--- js/modules/controllers/paletteui.js
+++ js/modules/controllers/paletteui.js
@@ -1,6 +1,22 @@
 import { userObjects } from "../view/userobjects.js";
-
+/**
+ * add on input
+ * add throttle debounce
+ * add colourspace conversion
+ * interface with data store
+ * trigger update on gradient maker etc
+ */
 export const paletteUi = {
+    _setOnChange() {
+        Object.keys(paletteUi.sliders).forEach((x,i) => x.oninput = (i) => this._sliderOnInput(i));
+        paletteUi.primaryPicker.oninput = () => {this._onchange()};
+        paletteUi.textPicker= () => {this._onchange()};
+        paletteUi.textLabel= () => {this._onchange()};
+        paletteUi.randomButton= () => {this._onchange()};
+        paletteUi.diceButton= () => {this._onchange()};
+        paletteUi.dieWrapperA= () => {this._onchange()};
+        paletteUi.dieWrapperB= () => {this._onchange()};
+    }, 
     _addPrimaryColour(newColour){
         const {hue, sat, lum, red, green, blue, hex, name} = newColour;
         const selectColourObject = {
@@ -12,13 +28,11 @@ export const paletteUi = {
         [userObjects.sliders['slider-a'].value, userObjects.sliders['slider-b'].value, userObjects.sliders['slider-c'].value] = selectColourObject[userObjects.buttons['colourspace-selector'].innerHTML.toLowerCase()];
         userObjects.pickers['primary-picker'].value = newColour.hex;
     },
-
     addColour(newColour){
         if (newColour.name === 'primary') {
             this._addPrimaryColour(newColour);
             return;
         }
-
         userObjects.pickers[newColour.name + '-picker'].value = newColour.hex;
     },
     getSliderColourObject(){
