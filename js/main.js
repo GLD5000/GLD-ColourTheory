82812f017afc26226b73a2eebff7d9ab301ca232
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-07-29 17:28:59 +0100

    Log palette data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { variantMaker } from './modules/controllers/variantmaker.js';
 import { paletteUi} from './modules/controllers/paletteui.js';
 import { gradientMaker } from './modules/controllers/gradientmaker.js';
+import { paletteData } from './modules/controllers/palettedata.js';
 //console.log(paletteData);
 paletteUi._init();
 console.log(paletteUi.userObjects());
+console.log(paletteData);
