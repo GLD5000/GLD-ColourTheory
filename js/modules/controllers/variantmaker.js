4d7c56411d4fa18960a89a3d1102c27fb7050799
GLD5000 <33987109+GLD5000@users.noreply.github.com>
2022-09-01 12:34:37 +0100

    Added tetradic modes to variantmaker

--- js/modules/controllers/variantmaker.js
+++ js/modules/controllers/variantmaker.js
@@ -2,18 +2,38 @@ import { colourObject } from "../utilities/colourobject.js";
 //import { callLogger } from "../utilities/utilities.js";
 import { paletteUi } from "./paletteui.js";
 export const variantMaker= {
-
+  _tetradicSwitchObject: {
+    square: {
+      'tetradicB': {name: 'tetradicB', hue: 90, operation: 'add'},
+      'tetradicC': {name: 'tetradicB', hue: 270, operation: 'add'},
+    },
+    rectangleA: {
+      'tetradicB': {name: 'tetradicB', hue: 60, operation: 'add'},
+      'tetradicC': {name: 'tetradicB', hue: 240, operation: 'add'},
+    },
+    rectangleB: {
+      'tetradicB': {name: 'tetradicB', hue: -60, operation: 'add'},
+      'tetradicC': {name: 'tetradicB', hue: 120, operation: 'add'},
+    },
+    
+  },
+  updateTetradicMode(tetradicMode){
+    variantMaker._swatchRecipeMap.set('tetradicB', this._tetradicSwitchObject[tetradicMode]['tetradicB'])
+    variantMaker._swatchRecipeMap.set('tetradicC', this._tetradicSwitchObject[tetradicMode]['tetradicC'])
+  },
   _swatchRecipeMap: new Map([
     ['analogousA', {name: 'analogousA', hue: -30, operation: 'add'}],
     ['analogousB', {name: 'analogousB', hue: 30, operation: 'add'}],
     ['triadicA', {name: 'triadicA', hue: -120, operation: 'add'}],
     ['triadicB', {name: 'triadicB', hue: 120, operation: 'add'}],
-    ['tetradicA', {name: 'tetradicA', hue: 90, operation: 'add'}],
-    ['tetradicB', {name: 'tetradicB', hue: 180, operation: 'add'}],
+    ['tetradicA', {name: 'tetradicA', hue: 180, operation: 'add'}],
+    ['tetradicB', {name: 'tetradicB', hue: 90, operation: 'add'}],
     ['tetradicC', {name: 'tetradicC', hue: 270, operation: 'add'}],
     ['monochromeA', {name: 'monochromeA', lum: -10, operation: 'add'}],
     ['monochromeB', {name: 'monochromeB', lum: 10, operation: 'add'}],
-    ['neutral', {name: 'neutral', sat: 0}]
+    ['neutral', {name: 'neutral', sat: 0}],
+   // ['splitA', {name: 'splitA', hue: 150, operation: 'add'}],
+   // ['splitB', {name: 'splitB', hue: 210, operation: 'add'}],
   ]), 
   updateVariants(primaryColour) {
     //const primaryColour = paletteUi.getColourObject('primary');
