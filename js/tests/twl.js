c2dbbcb8bc9944f8e0f80e073b772adb1d7d692b
Gareth Devlin <33987109+GLD5000@users.noreply.github.com>
2022-09-19 13:04:49 +0000

    Updated twl tests

--- js/tests/twl.js
+++ js/tests/twl.js
@@ -1,19 +1,17 @@
-import { paletteUi } from "./modules/controllers/paletteui.js";
-import { colourspace } from "./modules/utilities/colourmodules/colourspace.js";
-paletteUi._init();
+const colourObject = require("../modules/utilities/colourobject.js");
 
 const testColour = {name:'test colour', tint: Math.random().toFixed(2), warmth: Math.random().toFixed(2), lightness: Math.random().toFixed(2)};
 const colourArray = [];//[`t: ${testColour.tint} w: ${testColour.warmth} l: ${testColour.lightness} r: ${testColour.red} b: ${testColour.blue} g: ${testColour.green}`];
 //colourArray.push('_convertTwltoSrgb');
-colourspace._convertTwltoSrgb(testColour);
+colourObject._convertTwltoSrgb(testColour);
 colourArray.push(`t: ${testColour.tint} w: ${testColour.warmth} l: ${testColour.lightness} r: ${testColour.red} b: ${testColour.blue} g: ${testColour.green}`);
 //colourArray.push('_convertSrgbtoTwl');
-colourspace._convertSrgbtoTwl(testColour);
+colourObject._convertSrgbtoTwl(testColour);
 colourArray.push(`t: ${testColour.tint} w: ${testColour.warmth} l: ${testColour.lightness} r: ${testColour.red} b: ${testColour.blue} g: ${testColour.green}`);
 //colourArray.push('_convertTwltoSrgb');
-colourspace._convertTwltoSrgb(testColour);
+colourObject._convertTwltoSrgb(testColour);
 colourArray.push(`t: ${testColour.tint} w: ${testColour.warmth} l: ${testColour.lightness} r: ${testColour.red} b: ${testColour.blue} g: ${testColour.green}`);
 //colourArray.push('_convertSrgbtoTwl');
-colourspace._convertSrgbtoTwl(testColour);
+colourObject._convertSrgbtoTwl(testColour);
 colourArray.push(`t: ${testColour.tint} w: ${testColour.warmth} l: ${testColour.lightness} r: ${testColour.red} b: ${testColour.blue} g: ${testColour.green}`);
 console.log(colourArray.join('\n'));
