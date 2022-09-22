0626084b2d3822c25d5b53728c08c07730384c77
GLD5000 <garethlouisdevlin@gmail.com>
2022-09-22 21:35:33 +0100

    Fixed slider constraints

--- js/modules/utilities/colourmodules/constraints.js
+++ js/modules/utilities/colourmodules/constraints.js
@@ -4,12 +4,12 @@ export const constraints = {
     _constraintLookupB: {
         hue: (x) => clampRotate.rotate(x, 0, 360),
         sat: (x) => clampRotate.clamp(x, 0, 100),
-        lum: (x) => clampRotate.clamp(x, 15, 100),
+        lum: (x) => clampRotate.clamp(x, 0.1, 100),
         red: (x) => clampRotate.clamp(x, 0, 1),
         green: (x) => clampRotate.clamp(x, 0, 1),
         blue: (x) => clampRotate.clamp(x, 0, 1),
         tint: (x) => clampRotate.clamp(x, 0, 1),
         warmth: (x) => clampRotate.clamp(x, 0, 1),
-        lightness: (x) => clampRotate.clamp(x, 0.2, 1),
+        lightness: (x) => clampRotate.clamp(x, 0.001, 1),
       },
 }
