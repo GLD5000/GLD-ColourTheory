ff196b0191da1847a32527d75985e0f00f03a2e1
GLD5000 <garethlouisdevlin@gmail.com>
2022-10-05 19:35:00 +0100

    Added auto textcolour set contrast

--- js/modules/utilities/colourmodules/contrast.js
+++ js/modules/utilities/colourmodules/contrast.js
@@ -11,22 +11,99 @@ export const contrast = {
   makeContrastRating(ratio) {
     return ratio > 4.5 ? (ratio > 7 ? "AAA+" : "AA+") : "Low";
   },
-  _getTextColourMaxContrast(backgroundLuminance){
-    const backgroundLuminanceCutoff = 0.1791287847;
-    const backgroundLuminanceIsAboveCutoff = backgroundLuminance > backgroundLuminanceCutoff;
-    const textColour = backgroundLuminanceIsAboveCutoff? "#000000" : "#ffffff";
-    const textLuminance = backgroundLuminanceIsAboveCutoff? 0 : 1;
-    const textContrast = contrast.getContrastRatio([textLuminance, backgroundLuminance])
-    return [textColour, textContrast]
+  _luminanceAboveCutoff(luminance) {
+    const luminanceCutoff = 0.1791287847;
+    return luminance > luminanceCutoff;
+  },
+  _convertLuminanceToSrgbDecimal(luminance) {
+    console.log(`luminance ${luminance}`);
+
+    const luminanceThreshold = 0.04045 / 12.92;
+    const luminanceAboveThreshold = luminance > luminanceThreshold;
+    if (luminanceAboveThreshold) {
+      const srgbDecimal = 1.055 * luminance ** (1 / 2.4) - 0.055;
+
+      return srgbDecimal;
+    }
+    const srgbDecimal = luminance * 12.92;
+
+    return srgbDecimal;
+  },
+  _convertSrgbDecimalToHex(srgbDecimal) {
+    const hexString =
+      "#" +
+      Math.round(srgbDecimal * 255)
+        .toString(16)
+        .repeat(3);
+    return hexString;
+  },
+  _convertLuminanceToHex(luminance) {
+    const srgbDecimal = contrast._convertLuminanceToSrgbDecimal(luminance);
+    console.log(`srgbDecimal ${srgbDecimal}`);
+    const hexString = contrast._convertSrgbDecimalToHex(srgbDecimal);
+    return hexString;
+  },
+  _clampLuminance(luminance) {
+    return Math.min(1, Math.max(0, luminance));
+  },
+  _calculateMinLuminance(maxLuminance, targetContrast) {
+    const minLuminance =
+      (-0.05 * targetContrast + maxLuminance + 0.05) / targetContrast;
+    return contrast._clampLuminance(minLuminance);
+  },
+  _calculateMaxLuminance(minLuminance, targetContrast) {
+    const maxLuminance = targetContrast * (minLuminance + 0.05) - 0.05;
+    console.log(`maxLuminance ${maxLuminance}`);
+
+    return contrast._clampLuminance(maxLuminance);
+  },
+  _calculateTargetLuminance(backgroundLuminance, targetContrast) {
+    const backgroundLuminanceIsMax =
+      contrast._luminanceAboveCutoff(backgroundLuminance);
+    console.log(`backgroundLuminanceIsMax ${backgroundLuminanceIsMax}`);
+    const textLuminance = backgroundLuminanceIsMax
+      ? contrast._calculateMinLuminance(backgroundLuminance, targetContrast)
+      : contrast._calculateMaxLuminance(backgroundLuminance, targetContrast);
+    return textLuminance;
+  },
+  _getTextColourSetContrast(backgroundLuminance, targetContrast) {
+    console.clear();
+    console.log(`backgroundLuminance ${backgroundLuminance}`);
+    console.log(`targetContrast ${targetContrast}`);
+    const textLuminance = contrast._calculateTargetLuminance(
+      backgroundLuminance,
+      targetContrast
+    );
+    console.log(`textLuminance ${textLuminance}`);
+
+    const textColour = contrast._convertLuminanceToHex(textLuminance);
+    console.log(`textColour ${textColour}`);
+    const textContrast = contrast.getContrastRatio([
+      textLuminance,
+      backgroundLuminance,
+    ]);
+    return [textColour, textContrast];
+  },
+  _getTextColourMaxContrast(backgroundLuminance) {
+    const backgroundLuminanceIsAboveCutoff =
+      contrast._luminanceAboveCutoff(backgroundLuminance);
+    const textColour = backgroundLuminanceIsAboveCutoff ? "#000000" : "#ffffff";
+    const textLuminance = backgroundLuminanceIsAboveCutoff ? 0 : 1;
+    const textContrast = contrast.getContrastRatio([
+      textLuminance,
+      backgroundLuminance,
+    ]);
+    return [textColour, textContrast];
   },
   _autoTextColour(backgroundColour, targetContrast = null) {
     const backgroundLuminance = backgroundColour.relativeLuminance;
-    if (targetContrast === null) 
-    // const contrastBlack = contrast.getContrastRatio([0, relativeLuminance]);
-    // const contrastWhite = contrast.getContrastRatio([1, relativeLuminance]);
-    // const autoColour = contrastBlack > contrastWhite ? "#000000" : "#ffffff";
-    // const autoContrast = Math.max(contrastBlack, contrastWhite);
-    return contrast._getTextColourMaxContrast(backgroundLuminance);
+    if (targetContrast === null)
+      return contrast._getTextColourMaxContrast(backgroundLuminance);
+
+    return contrast._getTextColourSetContrast(
+      backgroundLuminance,
+      targetContrast
+    );
   },
   makeTextColour(textColour = null, backgroundColour = null) {
     if (backgroundColour == null) return "No Background Colour Found"; //if background colour == null return
@@ -57,5 +134,4 @@ export const contrast = {
     );
     return returnColour;
   },
-  
 };
